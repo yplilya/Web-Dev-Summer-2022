0d04a78b5d2f2f6be681827783966935e8e752b9
yplilya <ypl2002ilya@gmail.com>
2022-06-27 13:10:38 +0200

    added logics for the counters + created the list of posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/Button/MyButton";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript', body: 'Description'},
+        {id: 3, title: 'Javascript', body: 'Description'},
+    ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <form>
+              <input type="text" placeholder="Name of the post"/>
+              <input type="text" placeholder="Post text"/>
+              <MyButton>Create post</MyButton>
+          </form>
+          <PostList posts={posts} title={"List of posts"}/>
+      </div>
   );
 }
 
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PostItem = (props) => {
+    return (
+        <div>
+            <div className="post">
+                <div className="post__content">
+                    <strong>{props.post.id}. {props.post.title}</strong>
+                    <div>
+                        {props.post.body}
+                    </div>
+                </div>
+                <div className="post__btns">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
